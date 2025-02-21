b9c2c4c705c82900432aa49dd8e7a5c7481dc572
Igor Vallinote Veloso <ig_v@hotmail.com>
2025-02-21 20:34:11 -0300

    financiamento.py

--- financiamento.py
+++ financiamento.py
@@ -24,7 +24,6 @@ class Financiamento():
     #operação de financiamento pelo sistema de amortização constante.           
     def sac(self):
         self.amortizacao = self.saldo_devedor / self.prazo #valor de pagamento do saldo devedor
-
         resultado = [] #lista para armazenar as iterações do processo
         n_parcela = 0 #contador de parcelas
         #status inicial do financiamento salvo como um json básico
@@ -39,9 +38,7 @@ class Financiamento():
         )
         #estrutura de cáclulo para financiamentos com períodos de carência e captalização de juros com acréscimo do juros capitalizado ao saldo devedor
         if self.capitalizacao_juros and self.acrescimo_saldo and self.carencia > 0:
-
             for i in range(0, self.carencia):
-
                 self.saldo_devedor = self.saldo_devedor * (1 + self.taxa_juros)             
                 n_parcela += 1
                 resultado.append(
@@ -57,7 +54,6 @@ class Financiamento():
             self.amortizacao = self.saldo_devedor / self.prazo
 
             for i in range(0, self.prazo):
-
                 juros = self.saldo_devedor * (self.taxa_juros)
                 parcela = self.amortizacao + juros 
                 self.saldo_devedor -= self.amortizacao
@@ -79,7 +75,6 @@ class Financiamento():
             saldo = self.saldo_devedor #necessidade de isolar o valor de referência do saldo devedor para a primeira parcela com juros capitalizado
 
             for i in range(0, self.carencia):
-
                 saldo = saldo * (1 + self.taxa_juros) #nessa modalidade o juros incide direto sobre o saldo devedor         
                 n_parcela += 1
                 resultado.append(
@@ -93,7 +88,6 @@ class Financiamento():
                 )
 
             for i in range(0, self.prazo):
-
                 if i == 0: #primeira parcela possui lógica distinta das demais
                     juros = (saldo * (1 + self.taxa_juros)) - self.saldo_devedor
                     parcela = self.amortizacao + juros 
@@ -113,9 +107,7 @@ class Financiamento():
                     juros = self.saldo_devedor * (self.taxa_juros)
                     parcela = self.amortizacao + juros 
                     self.saldo_devedor -= self.amortizacao
-
                     n_parcela += 1
-
                     resultado.append(
                         {
                             'Nº da parcela': n_parcela,
@@ -131,7 +123,6 @@ class Financiamento():
         #estrutura de cáclulo para financiamentos com períodos de carência
         elif self.carencia > 0:
             for i in range(0, self.carencia): #particulariedades do período de carência
-
                 juros = self.saldo_devedor * self.taxa_juros #valor do juros
                 n_parcela += 1 #acréscimo sequencial da parcela
                 resultado.append(
@@ -145,7 +136,6 @@ class Financiamento():
                 )
 
             for i in range(0, self.prazo): #cálculo padrão do financiamento
-
                 juros = self.saldo_devedor * self.taxa_juros
                 parcela = self.amortizacao + juros
                 self.saldo_devedor -= self.amortizacao
@@ -165,7 +155,6 @@ class Financiamento():
     
         else: 
             for i in range(0, self.prazo):
-                
                 juros = self.saldo_devedor * (self.taxa_juros)
                 parcela = self.amortizacao + juros 
                 self.saldo_devedor -= self.amortizacao
@@ -202,7 +191,6 @@ class Financiamento():
             saldo = self.saldo_devedor
 
             for i in range(0, self.carencia):
-                
                 self.saldo_devedor = self.saldo_devedor * (1 + self.taxa_juros) 
                 n_parcela += 1
                 resultado.append(
@@ -218,13 +206,10 @@ class Financiamento():
             parcela = self.saldo_devedor / self.fpv
 
             for i in range(0, self.prazo):
-
                 juros = self.saldo_devedor * (self.taxa_juros)                    
                 self.amortizacao = parcela - juros
                 self.saldo_devedor -= self.amortizacao
-
                 n_parcela += 1
-
                 resultado.append(
                     {
                         'Nº da parcela': n_parcela,
@@ -240,7 +225,6 @@ class Financiamento():
 
         elif self.carencia > 0:
             for i in range(0, self.carencia):
-
                 juros = self.saldo_devedor * self.taxa_juros
                 parcela = juros 
                 n_parcela += 1
@@ -253,17 +237,14 @@ class Financiamento():
                         'Valor Parcela': parcela    
                     }
                 )
-
+                
             parcela = self.saldo_devedor / self.fpv
 
             for i in range(0, self.prazo):
-
                 juros = self.saldo_devedor * (self.taxa_juros)                    
                 self.amortizacao = parcela - juros
                 self.saldo_devedor -= self.amortizacao
-
                 n_parcela += 1
-
                 resultado.append(
                     {
                         'Nº da parcela': n_parcela,
@@ -285,7 +266,6 @@ class Financiamento():
                 self.amortizacao = parcela - juros
                 self.saldo_devedor -= self.amortizacao
                 n_parcela += 1
-
                 resultado.append(
                     {
                         'Nº da parcela': n_parcela,
@@ -327,13 +307,11 @@ class Amortizacao(Financiamento):
 
         if self.sistema == "SAC":
             for i in range(inicio, dataframe.shape[0]):
-
                 if contador == 0:
                     dataframe.loc[i, 'Novo Saldo Devedor'] = dataframe.loc[i, 'Saldo Devedor'] - self.valor 
                     dataframe.loc[i, 'Novo Juros'] = dataframe.loc[i, 'Juros'] 
                     dataframe.loc[i, 'Valor'] = self.valor
                     dataframe.loc[i, 'Nova Parcela'] = dataframe.loc[i, 'Amortização'] + dataframe.loc[i, 'Novo Juros'] + self.valor
-
                     contador += 1
 
                 else:
@@ -348,14 +326,12 @@ class Amortizacao(Financiamento):
         
         else:
             for i in range(inicio, dataframe.shape[0]):
-
                 if contador == 0:
                     dataframe.loc[i, 'Novo Saldo Devedor'] = dataframe.loc[i, 'Saldo Devedor'] - self.valor 
                     dataframe.loc[i, 'Nova Amortização'] = dataframe.loc[i, 'Amortização']
                     dataframe.loc[i, 'Novo Juros'] = dataframe.loc[i, 'Juros'] 
                     dataframe.loc[i, 'Valor'] = self.valor
                     dataframe.loc[i, 'Nova Parcela'] = dataframe.loc[i, 'Valor Parcela']
-
                     contador += 1
 
                 else:
@@ -367,8 +343,9 @@ class Amortizacao(Financiamento):
 
                     if dataframe.loc[i, 'Novo Saldo Devedor'] < dataframe.loc[i, 'Nova Amortização']:
                         break
-
+                        
         return dataframe.fillna(0)
+        
     # simulação do impacto de amortizações mensais fixas na opção de amortizar no do financiamento. 
     def amortizacao_prazo(self, dataframe):
         dataframe['Novo Saldo Devedor'] = np.nan
@@ -384,7 +361,6 @@ class Amortizacao(Financiamento):
             fim = dataframe.shape[0] - parcelas_abatidas #o loop for ocorre em um intervalo menor devido à remoção de parcelas futuras
 
             for i in range(inicio, fim):
-        
                 if contador == 0:
                     dataframe.loc[i, 'Novo Saldo Devedor'] = dataframe.loc[i, 'Saldo Devedor'] - self.valor 
                     dataframe.loc[i, 'Novo Juros'] = dataframe.loc[i, 'Juros'] 
@@ -405,7 +381,6 @@ class Amortizacao(Financiamento):
             fim = dataframe.shape[0] - parcelas_abatidas
 
             for i in range(inicio, fim):
-
                 if contador == 0:
                     dataframe.loc[i, 'Novo Saldo Devedor'] = dataframe.loc[i, 'Saldo Devedor'] - self.valor 
                     dataframe.loc[i, 'Nova Amortização'] = dataframe.loc[i, 'Amortização']
